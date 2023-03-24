41c420c269a506266cbbc5f20035a980b575f839
Davi Taveira <davitaveira@gmail.com>
2023-03-24 11:12:40 -0300

    aula-04

---
+++ 20231-POOI-producao-Aula01/src/Aula04/Ex01.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aula04;
+
+/**
+ *
+ * @author Davi
+ */
+import java.util.Scanner;
+public class Ex01 {
+    
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        String nome;
+        String sobrenome;
+        
+        System.out.print("Informe o nome: ");
+        nome = leia.next();
+        
+        System.out.print("Informe o sobrenome: ");
+        sobrenome = leia.next();
+        
+        System.out.println(nome+" "+sobrenome);
+    }
+    
+}
