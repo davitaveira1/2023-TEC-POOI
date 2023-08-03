0e9ae4683c731e28d8419de6f8911df574f8562a
Davi Taveira <davitaveira@gmail.com>
2023-08-03 09:34:56 -0300

    aula-09

---
+++ 20231-POOI-producao-Aula01/src/Aula09/calculadora.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aula09;
+
+/**
+ *
+ * @author Davi
+ */
+public class calculadora {
+    
+    String modelo;
+    
+    void apresentarModelo(){
+        System.out.println("Modelo: "+modelo);
+    }
+    
+}
